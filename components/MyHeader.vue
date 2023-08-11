4ed83c4b661857d8bf8a27b58e6c0289e26cf232
stradox4u <stradox@gmail.com>
2023-08-11 23:01:21 +0100

    opened blog route, added date to blog cards in landing page

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -18,7 +18,7 @@ const handleClick = () => {
           <ui-my-link to="/portfolio" title="Portfolio" class="my-nav-item"></ui-my-link>
         </li>
         <li>
-          <ui-my-link to="" title="Blog" class="my-nav-item"></ui-my-link>
+          <ui-my-link to="/blog" title="Blog" class="my-nav-item"></ui-my-link>
         </li>
         <li>
           <ui-my-link to="" title="Contact" class="my-nav-item"></ui-my-link>
@@ -34,7 +34,7 @@ const handleClick = () => {
           <ul @click="handleClick" v-if="menuOpen" class="flex flex-col bg-myMidGray bg-opacity-90">
             <ui-my-link to="/" title="Home" class="my-nav-item px-10"></ui-my-link>
             <ui-my-link to="/portfolio" title="Portfolio" class="my-nav-item px-10"></ui-my-link>
-            <ui-my-link to="" title="Blog" class="my-nav-item px-10"></ui-my-link>
+            <ui-my-link to="/blog" title="Blog" class="my-nav-item px-10"></ui-my-link>
             <ui-my-link to="" title="Contact" class="my-nav-item px-10"></ui-my-link>
           </ul>
         </Transition>
