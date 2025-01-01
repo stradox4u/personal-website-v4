54ddc63cd096b9031a9dfbaf7bb5d7ee10e12d90
Odeh Adejoh <stradox@gmail.com>
2025-01-01 22:56:43 +0100

    padding update

--- components/RandomImages.vue
+++ components/RandomImages.vue
@@ -3,11 +3,11 @@ import { randomImages } from '../assets/randomImages';
 </script>
 
 <template>
-  <ul class="list-none mx-8 grid sm:grid-cols-3 grid-cols-1 gap-4">
+  <ul class="list-none mx-8 grid sm:grid-cols-3 grid-cols-1 gap-4 pt-16">
     <li v-for="image in randomImages" :key="image.id" class="flex flex-col gap-2 items-center">
       <nuxt-img :src="image['src-attribute']" class="object-cover aspect-square" sizes="sm:480px md:720px" format="webp"
       :alt="image['alt-text']" provider="imgix"/>
-      <p class="font-roboto text-sm text-myMidGray">{{ image.description }}</p>
+      <p class="font-roboto text-xs text-myMidGray">{{ image.description }}</p>
     </li>
   </ul>
 </template>
