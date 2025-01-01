30a0e374e6bfc40607bcd47c55b87fa052a8c953
Odeh Adejoh <stradox@gmail.com>
2025-01-01 23:09:52 +0100

    corrected typo in gdg abj 01 image url

--- assets/randomImages.ts
+++ assets/randomImages.ts
@@ -9,7 +9,7 @@ export const randomImages = [
   {
     id: 1,
     "alt-text": "GDG Abuja Technical Session Photo",
-    "src-attribute": "/gdg_abuja_01.jpg",
+    "src-attribute": "/gdg_abj_01.jpg",
     description: "A photo taken at a GDG Abuja technical session focused on building AI integrations with Google Cloud Platform.",
   }
 ]
